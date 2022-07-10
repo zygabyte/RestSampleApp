3f16fd90690dacf36ac12bfb6d239b59b1e165ab
Eseosa Ehioghae <eseosaehioghae@gmail.com>
2022-07-10 03:26:23 +0100

    configure application

--- RestSampleApp/Program.cs
+++ RestSampleApp/Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using RestSampleApp.Data;
+using RestSampleApp.Services.ItemService;
+using RestSampleApp.Services.UserService;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 // Add services to the container.
 
@@ -6,6 +12,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = configuration.GetConnectionString("DbConnectionString");
+
+builder.Services.AddDbContext<AppDbContext>(x =>
+{
+    x.UseNpgsql(connectionString);
+});
+
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IItemService, ItemService>();
 
 var app = builder.Build();
 
