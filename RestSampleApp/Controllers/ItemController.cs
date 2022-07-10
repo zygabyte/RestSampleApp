071484af70a9fd8b0a41c9998a26ec551d2cb0c0
Eseosa Ehioghae <eseosaehioghae@gmail.com>
2022-07-10 03:26:01 +0100

    add controllers

---
+++ RestSampleApp/Controllers/ItemController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestSampleApp.Models;
+using RestSampleApp.Services.ItemService;
+
+namespace RestSampleApp.Controllers
+{
+    [ApiController]
+    [Route("/api/item")]
+    public class ItemController : Controller
+    {
+        private readonly IItemService itemService;
+
+        public ItemController(IItemService itemService)
+        {
+            this.itemService = itemService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<long>> CreateItem([FromBody] ItemRequest itemRequest)
+        {
+            var result = await itemService.CreateAsync(itemRequest);
+            return result > 0 ? Ok(result) : BadRequest("Error creating item");
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<ItemResponse>> GetItems()
+        {
+            var result = itemService.GetAllAsync();
+            return Ok(result);
+        }
+    }
+}
---
+++ RestSampleApp/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestSampleApp.Models;
+using RestSampleApp.Services.UserService;
+
+namespace RestSampleApp.Controllers
+{
+    [ApiController]
+    [Route("/api/user")]
+    public class UserController : Controller
+    {
+        private readonly IUserService userService;
+
+        public UserController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<long>> CreateUser([FromBody] UserRequest userRequest)
+        {
+            var result = await userService.CreateAsync(userRequest);
+            return result > 0 ? Ok(result) : BadRequest("Error creating user");
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<UserResponse>> GetUsers()
+        {
+            var result = userService.GetAllAsync();
+            return Ok(result);
+        }
+        
+        [HttpPost("login")]
+        public async Task<ActionResult<IEnumerable<UserResponse>>> LogInAsync([FromBody] UserRequest userRequest)
+        {
+            var result = await userService.LoginAsync(userRequest);
+            return result ? Ok(result) : BadRequest("Invalid credentials");
+        }
+    }
+}
